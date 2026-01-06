156a0aab39d00848b7ee03b1e447f6416bd74e45
thakurankit858 <thakurankit858@gmail.com>
2026-01-06 08:31:43 +0530

    header image issue solve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
+import headerImg from '../assets/header_img.png';
 
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex 
-        items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} 
+        items-center w-full overflow-hidden' style={{ backgroundImage: `url(${headerImg})` }} 
         id='Header'>
         
         <Navbar />
